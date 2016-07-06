5c9fdf4c8468fb55a640b063850f39cd22bcf823
Alessandro Belini <alessandro.belini@camtwo.com.br>
2016-07-06 11:11:12 -0300

    master [refactor]

--- extractor.py
+++ extractor.py
@@ -1,14 +1,37 @@
+import re
 import os
 import json
 import subprocess
+from distutils.version import LooseVersion
 
 from model import Subtitle, VxException
-from validation import verify_tools
+
+tools = ('mkvmerge', 'mkvextract')
+
+
+def check_tool(tool):
+	try:
+		search = re.search(r'v[\d.]+', subprocess.check_output([tool, '-V'], universal_newlines=True))
+		version = search and search.group()
+		if version and LooseVersion(version) < LooseVersion('v9.2.0'):
+			raise VxException('{!r} must have the version v9.2.0 or newer.'.format(tool))
+	except FileNotFoundError:
+		raise VxException('{0!r} Not found. Please install {0!r} in v9.2.0 version or newer.'.format(tool))
+
+
+def verify_tools(function):
+	def inner(*args, **kwargs):
+		for tool in tools:
+			check_tool(tool)
+
+		return function(*args, **kwargs)
+
+	return inner
 
 
 @verify_tools
 def find_in(video, model):
-	info = json.loads(subprocess.check_output(['mkvmerge', '-i', '-F', 'json', video], universal_newlines=True))
+	info = json.loads(subprocess.check_output([tools[0], '-i', '-F', 'json', video], universal_newlines=True))
 
 	if not info.get('container').get('recognized'):
 		raise VxException('{!r} unsupported file'.format(video))
@@ -23,7 +46,7 @@ def extract_subtitles(video, extrapath=''):
 	for sub in subtitles:
 		sub.concat_track_id = len(subtitles) > 1
 		out_filename = os.path.join(extrapath, sub.filename)
-		subprocess.call(['mkvextract', 'tracks', video, '{}:{}'.format(sub.track_id, out_filename)])
+		subprocess.call([tools[1], 'tracks', video, '{}:{}'.format(sub.track_id, out_filename)])
 
 	if not subtitles:
 		print('{!r} does not have tracks with subtitles'.format(video))
