8ff8e9515cc8ea4384226c6492b71b5729c8d1fc
Alessandro Belini <alessandro.belini@camtwo.com.br>
2016-07-06 11:01:12 -0300

    master [message install version of mkv tools]

--- validation.py
+++ validation.py
@@ -4,7 +4,7 @@ from distutils.version import LooseVersion
 
 from model import VxException
 
-tools = ('mkvmerge', 'mkvextract')
+tools = ('mkvmergej', 'mkvextract')
 
 
 def check_tool(tool):
@@ -12,10 +12,9 @@ def check_tool(tool):
 		search = re.search(r'v[\d.]+', subprocess.check_output([tool, '-V'], universal_newlines=True))
 		version = search and search.group()
 		if version and LooseVersion(version) < LooseVersion('v9.2.0'):
-			raise VxException('{!r} possui versão menor que v9.2.0'.format(tool))
-	except FileNotFoundError as e:
-		# TODO message
-		raise VxException(e.args[1])
+			raise VxException('{!r} must have the version v9.2.0 or newer.'.format(tool))
+	except FileNotFoundError:
+		raise VxException('{0!r} Not found. Please install {0!r} in v9.2.0 version or newer.'.format(tool))
 
 
 def verify_tools(function):
